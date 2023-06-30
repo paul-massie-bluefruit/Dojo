9a979dc99356871ecca8309e2c8ff82e1d4a4179
paul-massie-bluefruit <88719077+paul-massie-bluefruit@users.noreply.github.com>
2023-06-30 16:58:37 +0100

    yatzee rules

--- Yatzee.py
+++ Yatzee.py
@@ -15,14 +15,14 @@ import random
 class Dice:
     def __init__(self, sides, current_value):
         self.sides = 6
-        self. current_value = 0
+        self.current_value = 0
 
     def roll(self):
         self.current_value = random.randint(1, 6)
         print(self.current_value)
 
 di = Dice(6,0)
-Dice.roll(1)
+di.roll(1)
     
 
 
