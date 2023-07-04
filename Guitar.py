ed7b4e5af7d8f12b25827fd9f674629defa1db4a
paul-massie-bluefruit <88719077+paul-massie-bluefruit@users.noreply.github.com>
2023-07-04 11:07:21 +0100

    parent and child classs for a guitar

---
+++ Guitar.py
@@ -0,0 +1,28 @@
+#parent class
+class Guitar:
+    def __init__(self, n_strings = 6):
+        self.n_strings = n_strings
+        self.play()
+        self.__cost = 50
+    def play(self):
+        print("pam pam pam pam pam pam ")
+
+class BassGuitar(Guitar):
+    pass
+
+
+#child class
+class ElectricGuitar(Guitar):
+    def __init__(self):
+        super().__init__(n_strings = 8)
+        self.colour = ("000000", "FFFFFF")
+
+    def playLoader(self):
+        print("pam pam pam pam pam pam ".upper())
+    def __secret(self):
+        print("this guitar cost me £", self._Guitar__cost, "only!")
+
+my_guitar = ElectricGuitar()
+my_guitar._ElectricGuitar__secret()
+print("my base has ", BassGuitar(4).n_strings, "Strings")
+print("my electric has ", ElectricGuitar().n_strings, "Strings")
